106f659f4620a15a4c72e84ef758e611c3367c3d
Oluwanifemi OYEWOLE <oluwanifemi.oyewole@pau.edu.ng>
2021-10-31 18:12:07 +0100

    3

---
+++ Week 2 Excercise 4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	char myInitial = 'N';
+	char myName[] = "Nifemi Oyewole";
+	printf("Your name is = %s\n", myName);
+	printf("The initial of your firstname is = %c\n", myInitial);
+	return 0;
+}
