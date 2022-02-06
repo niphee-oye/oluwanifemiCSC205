6c42c43ba7b35227c1e9433e453ddfcf5b177103
niphee-oye <oluwanifemi.oyewole@pau.edu.ng>
2022-02-06 21:52:12 +0100

    Week4Project-1

--- ClassProject1/Print Next 6 chars.c
+++ ClassProject1/Print Next 6 chars.c
@@ -1,12 +1,57 @@
 #include <stdio.h>
-int main() 
+#include <math.h>
+
+int main()
 {
-    char c;
-    printf("Enter a character in the alphabets between 'a' and 'j'.\n");
-    scanf("%c", &c);
- 	
- 	for ( c >= 'a'; c <= 'j'; ++c )
-     	printf("%c", c);
- 
-    return 0;
+	char all[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
+	int n; char j;
+	
+	up:
+		printf("Enter a letter between A and J: "); 
+		scanf("%c", &j);
+		
+		if (j == 'A' || j == 'a')
+		{n = 0;}
+		
+		else if (j == 'B' || j == 'b')
+		{n = 1;}
+		
+		else if (j == 'C' || j == 'c')
+		{n = 2;}
+		
+		else if (j == 'D' || j == 'd')
+		{n = 3;}
+		
+		else if (j == 'E' || j =='e')
+		{n = 4;}
+		
+		else if (j == 'F' || j == 'f')
+		{n = 5;}
+		
+		else if (j == 'G' || j == 'g')
+		{n = 6;}
+		
+		else if (j == 'H' || j == 'h')
+		{n = 7;}
+		
+		else if (j == 'I' || j == 'i')
+		{n = 8;}
+		
+		else if (j == 'J' || j == 'j')
+		{n = 9;}
+		
+		else
+		{
+		printf("Invalid input! \n\n");
+		goto up;
+		}
+		
+	
+	for (int i = n; i < n+7; i++)
+	{
+		printf("%c ", all[i]);
+	}
+	printf("\n\n");
+	
+	return 0;
 }
